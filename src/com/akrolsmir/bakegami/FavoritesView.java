c7ab56c15102432eb4fc1d0bc322ae43f2b856d1
Austin Chen <akrolsmir@gmail.com>
2014-05-21 03:41:29 -0700

    Refactor GridView logic into FavoritesView

---
+++ src/com/akrolsmir/bakegami/FavoritesView.java
@@ -0,0 +1,110 @@
+package com.akrolsmir.bakegami;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class FavoritesView extends GridView {
+
+	public FavoritesView(final Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		//		final GridView gv = (GridView) findViewById(R.id.favorites);
+		final GridViewAdapter gva = new GridViewAdapter(context);
+		this.setAdapter(gva);
+		this.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+				Intent intent = new Intent();
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setDataAndType(
+						Uri.parse("file://" + WallpaperManager.getFavorites().get(position)),
+						"image/*");
+				context.startActivity(intent);
+			}
+		});
+		this.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE_MODAL);
+		this.setMultiChoiceModeListener(new MultiChoiceModeListener() {
+
+			@Override
+			public void onItemCheckedStateChanged(ActionMode mode, int position, long id,
+					boolean checked) {
+				// Here you can do something when items are selected/de-selected,
+				// such as update the title in the CAB
+			}
+
+			@Override
+			public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+				SparseBooleanArray checked = getCheckedItemPositions();
+				// Respond to clicks on the actions in the CAB
+				switch (item.getItemId()) {
+				case R.id.menu_item_delete:
+					//TODO confirm discard
+					for (int i = 0; i < getAdapter().getCount(); i++) {
+						if (checked.get(i)) {
+							WallpaperManager.removeFavorite(i);
+						}
+					}
+					Toast.makeText(context, getCheckedItemCount() + " unfavorited.",
+							Toast.LENGTH_LONG).show();
+					gva.notifyDataSetChanged();
+					mode.finish();
+					return true;
+				case R.id.menu_item_share:
+					Intent intent = new Intent();
+					intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+					intent.setType("image/*");
+
+					ArrayList<Uri> files = new ArrayList<Uri>();
+					for (int i = 0; i < getAdapter().getCount(); i++) {
+						if (checked.get(i)) {
+							files.add(Uri.fromFile(new File(WallpaperManager.getFavorites().get(i))));
+						}
+					}
+
+					intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, files);
+					context.startActivity(intent);
+					mode.finish();
+					return true;
+				default:
+					return false;
+				}
+			}
+
+			@Override
+			public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+				// Inflate the menu for the CAB
+				MenuInflater inflater = mode.getMenuInflater();
+				inflater.inflate(R.menu.gridview_menu, menu);
+				return true;
+			}
+
+			@Override
+			public void onDestroyActionMode(ActionMode mode) {
+				// Here you can make any necessary updates to the activity when
+				// the CAB is removed. By default, selected items are deselected/unchecked.
+			}
+
+			@Override
+			public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+				// Here you can perform updates to the CAB due to
+				// an invalidate() request
+				return false;
+			}
+		});
+	}
+}
--- src/com/akrolsmir/bakegami/GridViewAdapter.java
+++ src/com/akrolsmir/bakegami/GridViewAdapter.java
@@ -6,13 +6,9 @@ import java.io.File;
 import java.util.List;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.Checkable;
 import android.widget.ImageView;
@@ -20,140 +16,100 @@ import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 
 final class GridViewAdapter extends BaseAdapter {
-  private final Context context;
-  private List<String> favorites;
+	private final Context context;
+	private List<String> favorites;
 
-  public GridViewAdapter(Context context) {
-    this.context = context;
+	public GridViewAdapter(Context context) {
+		this.context = context;
+		this.favorites = WallpaperManager.getFavorites();
+	}
 
-    favorites = WallpaperManager.getFavorites();
-  }
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		SquaredImageView view = (SquaredImageView) convertView;
+		if (view == null) {
+			view = new SquaredImageView(context);
+			view.setScaleType(CENTER_CROP);
+		}
 
-  @Override public View getView(int position, View convertView, ViewGroup parent) {
-    SquaredImageView view = (SquaredImageView) convertView;
-    if (view == null) {
-      view = new SquaredImageView(context);
-      view.setScaleType(CENTER_CROP);
-    }
+		// Get the image URL for the current position.
+		String uri = getItem(position);
 
-    // Get the image URL for the current position.
-    String uri = getItem(position);
+		// Trigger the download of the URL asynchronously into the image view.
+		Picasso.with(context) //
+		.load(new File(uri)) //
+		//.placeholder(R.drawable.placeholder) //
+		//.error(R.drawable.error) //
+		.fit().centerCrop().into(view);
 
-    // Trigger the download of the URL asynchronously into the image view.
-    Picasso.with(context) //
-        .load(new File(uri)) //
-        //.placeholder(R.drawable.placeholder) //
-        //.error(R.drawable.error) //
-        .fit()
-        .centerCrop()
-        .into(view);
-    
-    view.setBackgroundResource(R.drawable.gridview_background);
+		view.setBackgroundResource(R.drawable.gridview_background);
 
-    return view;
-  }
-  
-  @Override
+		return view;
+	}
+
+	@Override
 	public void notifyDataSetChanged() {
 		favorites = WallpaperManager.getFavorites();
 		super.notifyDataSetChanged();
 	}
-  
-//  private final int[] CHECKED_STATE_SET = { android.R.attr.state_checked };
-//
-//	public View getView(final int position, final View convertView, final ViewGroup parent) {
-//		ImageView imageView = (ImageView) convertView;
-//		if(imageView == null) {
-//			imageView = new ImageView(context) {
-//				
-//			    @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//			        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-//			        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
-//			      }
-//			    
-//				@Override
-//				public int[] onCreateDrawableState(int extraSpace) {
-//					final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
-//					if (((AbsListView) parent).isItemChecked(position)) {
-//						mergeDrawableStates(drawableState, CHECKED_STATE_SET);
-//					}
-//					return drawableState;
-//				}
-//			};
-//			imageView.setScaleType(CENTER_CROP);
-//		}
-//		
-//		// Get the image URL for the current position.
-//		String uri = getItem(position);
-//
-//		// Trigger the download of the URL asynchronously into the image view.
-//		Picasso.with(context) //
-//		.load(new File(uri)) //
-//		//.placeholder(R.drawable.placeholder) //
-//		//.error(R.drawable.error) //
-//		.fit().centerCrop().into(imageView);
-//				
-//				
-//		imageView.setBackgroundResource(R.drawable.gridview_background);
-////		imageView.setScaleType(ImageView.ScaleType.CENTER);
-//
-//		return imageView;
-//	}
 
-  @Override public int getCount() {
-    return favorites.size();
-  }
-
-  @Override public String getItem(int position) {
-    return favorites.get(position);
-  }
-
-  @Override public long getItemId(int position) {
-    return position;
-  }
-  
-  /** An image view which always remains square with respect to its width. */
-  final class SquaredImageView extends ImageView implements Checkable {
-    public SquaredImageView(Context context) {
-      super(context);
-    }
-
-    public SquaredImageView(Context context, AttributeSet attrs) {
-      super(context, attrs);
-    }
-
-    @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-      super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-      setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
-    }
-    
-    private boolean checked = false;
-    private final int[] CHECKED_STATE_SET = { android.R.attr.state_checked };
-
-    @Override
-	public int[] onCreateDrawableState(int extraSpace) {
-         final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
-         if (isChecked())
-             mergeDrawableStates(drawableState, CHECKED_STATE_SET);
-         return drawableState;
-    }
-    
 	@Override
-	public void setChecked(boolean checked) {
-		this.checked = checked;
-		refreshDrawableState();
+	public int getCount() {
+		return favorites.size();
 	}
 
 	@Override
-	public boolean isChecked() {
-		return checked;
+	public String getItem(int position) {
+		return favorites.get(position);
 	}
 
 	@Override
-	public void toggle() {
-		setChecked(!checked);
+	public long getItemId(int position) {
+		return position;
 	}
-  }
+
+	/** An image view which always remains square with respect to its width. */
+	final class SquaredImageView extends ImageView implements Checkable {
+		public SquaredImageView(Context context) {
+			super(context);
+		}
+
+		public SquaredImageView(Context context, AttributeSet attrs) {
+			super(context, attrs);
+		}
+
+		@Override
+		protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+			super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+			setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+		}
+
+		// Makes this ImageView checkable
+		private boolean checked = false;
+		private final int[] CHECKED_STATE_SET = { android.R.attr.state_checked };
+
+		@Override
+		public int[] onCreateDrawableState(int extraSpace) {
+			final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+			if (isChecked())
+				mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+			return drawableState;
+		}
+
+		@Override
+		public void setChecked(boolean checked) {
+			this.checked = checked;
+			refreshDrawableState();
+		}
+
+		@Override
+		public boolean isChecked() {
+			return checked;
+		}
+
+		@Override
+		public void toggle() {
+			setChecked(!checked);
+		}
+	}
 }
-
-
