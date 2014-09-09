f90a2084462f27be21313abe363907bfe7985638
gmkarthik64 <gmkarthik64@gmail.com>
2014-09-09 00:17:28 -0700

    Multiple subreddits and minor bugfixes
    Fixed nsfw
    Fixed issue with "https"

--- src/com/akrolsmir/bakegami/SettingsActivity.java
+++ src/com/akrolsmir/bakegami/SettingsActivity.java
@@ -15,24 +15,27 @@ public class SettingsActivity extends PreferenceActivity implements
         addPreferencesFromResource(R.xml.preferences);
     }
     
-	public static String KEY_PREF_SUBREDDIT = "pref_subreddit", 
-			KEY_PREF_SHOW_NSFW = "pref_show_nsfw";
+	public static String[] KEY_PREF_SUBREDDITS = {"sr0","sr1","sr2","sr3","sr4","sr5","sr6",
+		"sr7","sr8","sr9"}; 
+	public static String KEY_PREF_SHOW_NSFW = "pref_show_nsfw";
     
     @Override
 	public void onSharedPreferenceChanged(SharedPreferences sp, String key) {
     	// Set summary to be the user-description for the selected value
-    	if (key.equals(KEY_PREF_SUBREDDIT)) {
-			findPreference(key).setSummary("r/" + sp.getString(key, ""));
-			WallpaperManager.with(this).resetQueue();
-		}
+    		for( String k : KEY_PREF_SUBREDDITS)
+    			if (key.equals(k)) { 
+    				findPreference(key).setSummary("r/" + sp.getString(key, ""));
+				WallpaperManager.with(this).resetQueue();
+				break;
+    			}
 	}
     
-    public static String getSubreddit(Context context) {
-    	return with(context).getString(KEY_PREF_SUBREDDIT, "");
+    public static String getSubreddit(Context context, int index) {
+    	return with(context).getString(KEY_PREF_SUBREDDITS[index], "");
     }
     
     public static boolean showNSFW(Context context) {
-    	return with(context).getBoolean(KEY_PREF_SUBREDDIT, false);
+    	return with(context).getBoolean(KEY_PREF_SHOW_NSFW, false);
     }
     
     public static long getRefreshSeconds(Context context) {
@@ -50,7 +53,8 @@ public class SettingsActivity extends PreferenceActivity implements
                 .registerOnSharedPreferenceChangeListener(this);
         
         // Hackily updates the summary
-        findPreference(KEY_PREF_SUBREDDIT).setSummary("r/" + getSubreddit(this));
+	for( int i = 0; i < 10; i++ )
+        	findPreference(KEY_PREF_SUBREDDITS[i]).setSummary("r/" + getSubreddit(this,i));
     }
 
     @Override
