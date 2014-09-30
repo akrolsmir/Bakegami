b0e9d156c34d0326076f7580790f0fa23198df5b
gmkarthik64 <gmkarthik64@gmail.com>
2014-09-29 22:59:15 -0700

    Added back buttons from settings
    I swear these were supposed to be automatic

--- src/com/akrolsmir/bakegami/SettingsActivity.java
+++ src/com/akrolsmir/bakegami/SettingsActivity.java
@@ -1,11 +1,21 @@
 package com.akrolsmir.bakegami;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class SettingsActivity extends PreferenceActivity implements
 		OnSharedPreferenceChangeListener {
@@ -13,6 +23,33 @@ public class SettingsActivity extends PreferenceActivity implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        View homeBtn = findViewById(android.R.id.home);
+        if( homeBtn != null ){
+        	OnClickListener dismissClickListener = new OnClickListener(){
+        		@Override
+        		public void onClick(View v){
+        			SettingsActivity.this.finish();
+        		}
+        	};
+        	ViewParent homeBtnContainer = homeBtn.getParent();
+
+            // The home button is an ImageView inside a FrameLayout
+            if (homeBtnContainer instanceof FrameLayout) {
+                ViewGroup containerParent = (ViewGroup) homeBtnContainer.getParent();
+
+                if (containerParent instanceof LinearLayout) {
+                    // This view also contains the title text, set the whole view as clickable
+                    ((LinearLayout) containerParent).setOnClickListener(dismissClickListener);
+                } else {
+                    // Just set it on the home button
+                    ((FrameLayout) homeBtnContainer).setOnClickListener(dismissClickListener);
+                }
+            } else {
+                // The 'If all else fails' default case
+                homeBtn.setOnClickListener(dismissClickListener);
+            }
+        }
     }
     
 	public static String[] KEY_PREF_SUBREDDITS = {"sr0","sr1","sr2","sr3","sr4","sr5","sr6",
@@ -20,6 +57,50 @@ public class SettingsActivity extends PreferenceActivity implements
 	public static String KEY_PREF_SHOW_NSFW = "pref_show_nsfw";
 	public static String KEY_PREF_ALLOW_DATA = "pref_allow_data";
     
+	private void addBack(PreferenceScreen preferenceScreen){
+		final Dialog dialog = preferenceScreen.getDialog();
+		if (dialog != null) {
+			dialog.getActionBar().setDisplayHomeAsUpEnabled(true);
+			View homeBtn = dialog.findViewById(android.R.id.home);
+			if (homeBtn != null) {
+				OnClickListener dismissClickListener = new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						dialog.dismiss();
+					}
+				};
+				ViewParent homeBtnContainer = homeBtn.getParent();
+
+				// The home button is an ImageView inside a FrameLayout
+				if (homeBtnContainer instanceof FrameLayout) {
+					ViewGroup containerParent = (ViewGroup) homeBtnContainer
+							.getParent();
+
+					if (containerParent instanceof LinearLayout) {
+						// This view also contains the title text, set the whole
+						// view as clickable
+						((LinearLayout) containerParent)
+								.setOnClickListener(dismissClickListener);
+					} else {
+						// Just set it on the home button
+						((FrameLayout) homeBtnContainer)
+								.setOnClickListener(dismissClickListener);
+					}
+				} else {
+					// The 'If all else fails' default case
+					homeBtn.setOnClickListener(dismissClickListener);
+				}
+			}
+		}
+	}
+	@Override
+	public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference){
+		if(preference instanceof PreferenceScreen)
+		{
+			addBack((PreferenceScreen)preference);
+		}
+		return true;
+	}
     @Override
 	public void onSharedPreferenceChanged(SharedPreferences sp, String key) {
     	// Set summary to be the user-description for the selected value
@@ -58,6 +139,9 @@ public class SettingsActivity extends PreferenceActivity implements
         super.onResume();
         getPreferenceScreen().getSharedPreferences()
                 .registerOnSharedPreferenceChangeListener(this);
+        if(findPreference("nestedKey") != null && findPreference("nestedKey") instanceof PreferenceScreen){
+        	addBack((PreferenceScreen)findPreference("nestedKey"));
+        }
         
         // Hackily updates the summary
 	for( int i = 0; i < 10; i++ )
