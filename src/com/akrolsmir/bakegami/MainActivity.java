63bc1a3caffdd3bbc65f5e530d1b01209bdedbf2
Austin Chen <akrolsmir@gmail.com>
2014-10-02 18:25:28 -0700

    Remove help activity
    This reverts commit 0336234bbb7eb509895e627e12fd38921838ace6.
    
    Revert "Added "Help" and probably some other things but its 4 AM so whatever"
    
    This reverts commit c48246659250e15ab5cc87b3e5aa957f039a47d7.
    
    Revert some more things

--- src/com/akrolsmir/bakegami/MainActivity.java
+++ src/com/akrolsmir/bakegami/MainActivity.java
@@ -131,8 +131,6 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		menu.findItem(R.id.action_settings).setIntent(
 				new Intent(this, SettingsActivity.class));
-		menu.findItem(R.id.action_help).setIntent(
-				new Intent(this, ItemListActivity.class));
 		return true;
 	}
 
@@ -211,19 +209,4 @@ public class MainActivity extends Activity {
 			}
 		}
 	}
-	// public void updateConnectedFlags() {
-	// ConnectivityManager connMgr = (ConnectivityManager)
-	// getSystemService(Context.CONNECTIVITY_SERVICE);
-	//
-	// NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
-	// if (activeInfo != null && activeInfo.isConnected()) {
-	// wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
-	// mobileConnected = activeInfo.getType() ==
-	// ConnectivityManager.TYPE_MOBILE;
-	// } else {
-	// wifiConnected = false;
-	// mobileConnected = false;
-	// }
-	// }
-
 }
